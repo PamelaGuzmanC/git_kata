b7fc89d76a7bb4601b9d5a7cb6054a862e54d224
Claudia <pamelaguzmann@gmail.com>
2024-04-24 21:31:10 +0200

    change

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import pandas as pd
-df = pd.read_csv('data/titanic.csv')
-print(df.columns)
-print(df["sex"].unique())
-female_df = df[df["sex"]=="female"]
-print(female_df)
+def load_titanic_female():
+    df = pd.read_csv('data/titanic.csv')
+    print(df.columns)
+    print(df["sex"].unique())
+    female_df = df[df["sex"]=="male"]
+    return female_df
