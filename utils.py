1501c8f527d9fa7a8507301aa8bd169980d3459f
Claudia <pamelaguzmann@gmail.com>
2024-04-24 21:56:58 +0200

    new

--- utils.py
+++ utils.py
@@ -1,15 +1,1 @@
-import pandas as pd
-<<<<<<< HEAD
-df = pd.read_csv('/Users/pamelaguzman/git_kata/data/titanic.csv')
-print(df.columns)
-print(df["sex"].unique())
-female_df = df[df["sex"]=="female"]
-print(female_df)
-=======
-def load_titanic_female():
-    df = pd.read_csv('data/titanic.csv')
-    print(df.columns)
-    print(df["sex"].unique())
-    female_df = df[df["sex"]=="male"]
-    return female_df
->>>>>>> utils
+
